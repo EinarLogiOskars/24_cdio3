4e09f526054bbcfb617bbd0e0e290bebadcfe058
Einar Logi <einarlogi1@gmail.com>
2017-05-10 22:35:50 +0200

    working on updateUser js

--- 24_cdio3/WebContent/updateUser.js
+++ 24_cdio3/WebContent/updateUser.js
@@ -1,68 +1,70 @@
-var user;
+$(document).ready(function() {
 
-// Gets all userId's from the database.
-$.ajax({
-	url: "http://localhost:8080/24_cdio3/rest2/cdio3/userids",
-	method: "GET",
-	success: function(data) {
-		numbers = data;
-	},
-	error: function(error) {
-		console.log(error);
-	}
+	var numbers;
 
-});
-
-
-$("#UUButton").click(function() {	
-	
-	var userId = parseInt($('#userIDChosen').val());
-	
+	//	Gets all userId's from the database.
 	$.ajax({
-		url: "http://localhost:8080/24_cdio3/rest2/cdio3/users/" + userId,
-		method: "POST",
-		data: data,
-		contentType: "application/json",
+		url: "http://localhost:8080/24_cdio3/rest2/cdio3/userids",
+		method: "GET",
 		success: function(data) {
-			user = data;
-			$("#UUForm").load("updateUserForm");
-			$("#userId").val(user.userId);
-			$("#userName").val(user.userName);
-			$("#ini").val(user.ini);
-			$("#password").val(user.password);
-			$("#cpr").val(user.cpr);
-		    //$('#UUData').load(user);
+			numbers = data;
 		},
 		error: function(error) {
-			alert("Error: " + error);
+			console.log(error);
 		}
-		
-		
-	});
 
-	//Invalid input - mark with red border around ID input field...
-	$('input[type="number"]').keyup(function(){
-		var value = $(this).val();
-		if(value < 11 || value > 89) {
-			$(this).attr('class', 'w3-border w3-border-red w3-input');
-		}
-		else {
-			$(this).attr('class', 'w3-input');
-		}
 	});
 
 
-	// To validate whether the chosen id to delete exists.
-	function validateID() {
-		var nr = parseInt($('#delUserID').val());
-		for(i in numbers){
-			if (numbers[i] == nr){
-				alert("Are you sure you want to delete user: " + nr);
-				return true;
+	$("#UButton").click(function() {	
+		if(validateID()){
+			$("#UUDiv").load("updateUserForm.html");
+			var userId = parseInt($('#userIDChosen').val());
+			var user;
+			$.ajax({
+				url: "http://localhost:8080/24_cdio3/rest2/cdio3/users/" + userId,
+				method: "GET",
+				data: user,
+				contentType: "application/json",
+				success: function(data) {
+					user = data;
+					$("#userId").val(user.userId);
+					$("#userName").val(user.userName);
+					$("#ini").val(user.ini);
+					$("#password").val(user.password);
+					$("#cpr").val(user.cpr);
+					//$('#UUData').load(user);
+				},
+				error: function(error) {
+					alert("Error: " + error);
+				}
+
+			});			
+		}
+
+		//Invalid input - mark with red border around ID input field...
+		$('input[type="number"]').keyup(function(){
+			var value = $(this).val();
+			if(value < 11 || value > 89) {
+				$(this).attr('class', 'w3-border w3-border-red w3-input');
 			}
-		}	
-		alert("No user has that user ID...");
-		return false;
-	}
+			else {
+				$(this).attr('class', 'w3-input');
+			}
+		});
 
+
+		// To validate whether the chosen id to delete exists.
+		function validateID() {
+			var nr = parseInt($('#userIDChosen').val());
+			for(i in numbers){
+				if (numbers[i] == nr){
+					return true;
+				}
+			}	
+			alert("No user has that user ID...");
+			return false;
+		}
+
+	});
 });
